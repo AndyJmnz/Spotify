89c25cd94030361ed33e340b45c911c28fb59aee
Andy Jimenez <a17100657@gmail.com>
2024-09-11 07:59:48 -0600

    Spotify

---
+++ app/src/main/java/com/example/spotify/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.spotify
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Componentes tipo boton
+        val botonMusic = findViewById<Button>(R.id.btn_music)
+        val botonMedia = findViewById<Button>(R.id.btn_media)
+        val botonVideo = findViewById<Button>(R.id.btn_video)
+        /*botonMedia.setOnClickListener { v: View? ->
+            val intent = Intent(applicationContext, activity_music::class.java)
+            startActivity(intent)
+        }*/
+        botonMedia.setOnClickListener { v: View? ->
+            val intent = Intent(applicationContext, activity_media::class.java)
+            startActivity(intent)
+        }
+        botonVideo.setOnClickListener { v: View? ->
+            val intent = Intent(applicationContext, activity_video::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/spotify/activity_media.kt
@@ -0,0 +1,18 @@
+package com.example.spotify
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class activity_media : AppCompatActivity() {
+    private var play: Button? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_media)
+
+        play =
+    }
+}
